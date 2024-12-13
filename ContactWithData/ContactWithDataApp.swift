0926e08025f22a8fd4202852dc93669cee8fd217
Hall <1234@mail.com>
2024-12-13 11:29:59 +0800

    Initial Commit

---
+++ ContactWithData/ContactWithDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactWithDataApp.swift
+//  ContactWithData
+//
+//  Created by 郝明翰 on 2024/12/13.
+//
+
+import SwiftUI
+
+@main
+struct ContactWithDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
